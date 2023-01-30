334cff89401cc4c2a37759a3b96e0830ae447980
Carl Richard Theodor Schneider <dev.github@crtified.me>
2023-01-30 13:49:42 +0100

    flake.nix: Refactor to use nixosModules

--- flake.nix
+++ flake.nix
@@ -1,9 +1,39 @@
 {
-  outputs = {self}: {
-    zshPlugin = {
-      name = "zsh-johnnydecimal";
-      file = "johnnydecimal.zsh";
-      src = ./.;
+  outputs = { self }: {
+    nixosModules = {
+      hmModule = { config, lib, ... }:
+        with lib;
+        let cfg = config.programs.zsh.johnnydecimal;
+        in {
+          options = {
+            programs.zsh.johnnydecimal = {
+              enable = mkEnableOption "zsh-johnnydecimal";
+              basePath = mkOption {
+                type = types.path;
+                default = "~/johnny/";
+                description = "Path to the root of the J.D structure";
+              };
+            };
+          };
+
+          config = mkIf cfg.enable {
+            programs.zsh.plugins = [{
+              name = "zsh-johnnydecimal";
+              file = "johnnydecimal.zsh";
+              src = ./.;
+            }];
+
+            home.sessionVariables = {
+              "JOHNNYDECIMAL_BASE" = cfg.basePath;
+            };
+          };
+        };
+
+      globalModule = {config, ...}: {
+        home-manager.sharedModules = [ self.nixosModules.hmModule ];
+      };
+
+      default = self.nixosModules.globalModule;
     };
   };
 }
